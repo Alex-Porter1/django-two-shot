c52f4accbce8b47e3e29beaf58cdc9fcac1e0c63
Alex Porter <alpo1992@gmail.com.here>
2022-06-15 14:44:48 -0600

    Feature 5 complete

--- receipts/admin.py
+++ receipts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from receipts.models import ExpenseCategory, Receipt, Account
 # Register your models here.
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    pass
+
+
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class AccountAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Receipt, ReceiptAdmin)
+admin.site.register(ExpenseCategory, ExpenseCategoryAdmin)
+admin.site.register(Account, AccountAdmin)
---
+++ receipts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from receipts.views import (
+    ReceiptListView
+)
+
+urlpatterns = [
+    path("", ReceiptListView.as_view(), name="home")
+]

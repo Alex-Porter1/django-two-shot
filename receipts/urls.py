1375617d2a01e10a1666043cda12c396c4cad75f
Alex Porter <alpo1992@gmail.com.here>
2022-06-15 18:41:32 -0600

    Feature 12 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from receipts.views import (
+    AccountListView,
     ReceiptCreateView,
-    ReceiptListView
+    ReceiptListView,
+    ExpenseCategoryListView,
 )
 
 urlpatterns = [
     path("", ReceiptListView.as_view(), name="home"),
-    path("create/", ReceiptCreateView.as_view(), name="create_receipt")
+    path("create/", ReceiptCreateView.as_view(), name="create_receipt"),
+    path(
+        "categories/",
+        ExpenseCategoryListView.as_view(),
+        name="list_categories"),
+    path(
+        "accounts/",
+        AccountListView.as_view(), name="list_accounts"
+    ),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
-from receipts.models import Receipt
+from receipts.models import Account, ExpenseCategory, Receipt
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 # Create your views here.
@@ -25,3 +25,21 @@ class ReceiptCreateView(LoginRequiredMixin, CreateView):
         item.purchaser = self.request.user
         item.save()
         return redirect("home")
+
+
+class ExpenseCategoryListView(LoginRequiredMixin, ListView):
+    model = ExpenseCategory
+    template_name = "expense_categories/list.html"
+    context_object_name = "expensecategory"
+
+    def get_queryset(self):
+        return ExpenseCategory.objects.filter(owner=self.request.user)
+
+
+class AccountListView(LoginRequiredMixin, ListView):
+    model = Account
+    template_name = "accounts/list.html"
+    context_object_name = "accounts"
+
+    def get_queryset(self):
+        return Account.objects.filter(owner=self.request.user)
